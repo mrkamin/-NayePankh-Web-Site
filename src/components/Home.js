bb9857cd39437650916bd64f12601e290ec3c2f9
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-26 02:18:07 +0500

    Add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,11 @@ import { HomeBgImage } from './Varaible';
 import '../css/home.css';
 
 const Home = () => {
-  const homeContainer = useRef;
-  const homeContParagraph = useRef;
+  const homeContainer = useRef();
+  const homeContParagraph = useRef();
 
   useLayoutEffect(() => {
     const contex = gsap.context(() => {
-      gsap.from('.home-cont-paragraph', {
-        duration: 3,
-        y: '20%',
-        ease: 'bounce',
-      });
       gsap.from(homeContParagraph.current, {
         duration: 3,
         y: '20%',
@@ -33,7 +28,10 @@ const Home = () => {
         <div className="position-absolute license">
           <h4>UP GOVT. | 80G&12A Registered</h4>
         </div>
-        <div className="home-cont-paragraph position-absolute text-white d-flex flex-column gap-5">
+        <div
+          className="home-cont-paragraph position-absolute text-white d-flex flex-column gap-5"
+          ref={homeContParagraph}
+        >
           <h1 className="fw-bolder">
             It&apos;s that easy to
             {' '}
@@ -49,7 +47,7 @@ const Home = () => {
             <br />
             you can-Be it Money, skill or Your time
           </p>
-          <button type="button" className="btn bg-white">
+          <button type="button" className="btn bg-white donate-btn">
             Donate Now
           </button>
         </div>
