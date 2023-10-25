ea541e2316373e4f8bd880fb962714ce2ebe22d2
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-26 02:00:50 +0500

    Add gsap animation to the home paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
 import { HomeBgImage } from './Varaible';
 import '../css/home.css';
 
-const Home = () => (
-  <>
-    <div className="home-container">
-      <div className="home-container-back-image bg-dark">
-        <img src={HomeBgImage} alt="HomeLogo" className="home-bg-image" />
+const Home = () => {
+  const homeContainer = useRef;
+  const homeContParagraph = useRef;
+
+  useLayoutEffect(() => {
+    const contex = gsap.context(() => {
+      gsap.from('.home-cont-paragraph', {
+        duration: 3,
+        y: '20%',
+        ease: 'bounce',
+      });
+      gsap.from(homeContParagraph.current, {
+        duration: 3,
+        y: '20%',
+        ease: 'bounce',
+      });
+    }, homeContainer);
+    return () => contex.revert();
+  }, []);
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-container-back-image bg-dark">
+          <img src={HomeBgImage} alt="HomeLogo" className="home-bg-image" />
+        </div>
+        <div className="overlay position-absolute" />
+        <div className="position-absolute license">
+          <h4>UP GOVT. | 80G&12A Registered</h4>
+        </div>
+        <div className="home-cont-paragraph position-absolute text-white d-flex flex-column gap-5">
+          <h1 className="fw-bolder">
+            It&apos;s that easy to
+            {' '}
+            <br />
+            {' '}
+            bring a Smile on
+            {' '}
+            <br />
+            Their Faces
+          </h1>
+          <p>
+            We don&apos;t ask for much, just help us with what
+            <br />
+            you can-Be it Money, skill or Your time
+          </p>
+          <button type="button" className="btn bg-white">
+            Donate Now
+          </button>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 export default Home;
