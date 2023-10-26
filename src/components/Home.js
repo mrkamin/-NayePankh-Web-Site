c7fb6f102b331c41a5b264a7ca364fb1fea645c9
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-26 22:00:15 +0500

    Make Home Container sect two responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import IMG1 from '../assets/IMG-20231025-WA0036.jpg';
+import IMG2 from '../assets/IMG-20231025-WA0046.jpg';
 import { Aboutusimg } from './Varaible';
 import '../css/home.css';
 
@@ -35,80 +36,104 @@ const Home = () => {
 
   return (
     <>
-      <div className="home-container">
-        <div className="home-container-back-image bg-dark">
-          <img src={IMG1} alt="HomeLogo" className="home-bg-image" />
-        </div>
-        <div className="overlay position-absolute" />
-        <div className="position-absolute license">
-          <h4>UP GOVT. | 80G&12A Registered</h4>
-        </div>
-        <div
-          className="home-cont-paragraph position-absolute text-white d-flex flex-column gap-5"
-          ref={homeContParagraph}
-        >
-          <h1 className="homp-paragraph">
-            It&apos;s that easy to
-            {' '}
-            <br />
-            {' '}
-            bring a Smile on
-            {' '}
-            <br />
-            Their Faces
-          </h1>
-          <p className="fs-5">
-            We don&apos;t ask for much, just help us with what
-            <br />
-            you can-Be it Money, skill or Your time
-          </p>
-          <button type="button" className="btn bg-white donate-btn">
-            Donate Now
-          </button>
-        </div>
-      </div>
-      <div
-        className="container grid home-about-container"
-        ref={homeAboutContainer}
-      >
-        <div ref={homeContParagraph}>
-          <img src={Aboutusimg} alt="aboutimg" />
-        </div>
-        <div className="d-flex flex-column  justify-content-center gap-3">
-          <h5>ABOUT US</h5>
-          <h1 className="aboutus-paragraph">
-            Think global,
-            {' '}
-            <br />
-            {' '}
-            Act local.
-          </h1>
-          <p>
-            &ldquo;NayePankh Foundation&ldquo; is a non governmental
-            organisation with a strong desire to help the society and make it a
-            better place for all, by doing everything in our power and to make
-            our vision successful we would require your vital support. Service
-            to mankind is the service to god. Let&apos;s revolutionise the
-            society together!.
-          </p>
-          <div className="d-flex justify-content-between">
+      <section className="home-cont-sect-one w-100">
+        <div className="home-cont-sect-one-items w-100">
+          <div className="home-cont-sect-one-items-item-one w-100">
+            <img
+              src={IMG1}
+              alt="HomeLogo"
+              className="home-cont-sect-one-items-item-one-img w-100"
+            />
+          </div>
+          <div className="overlay position-absolute w-100" />
+          <div className="home-cont-sect-one-items-item-two position-absolute">
+            <h4>UP GOVT. | 80G&12A Registered</h4>
+          </div>
+          <div
+            className="home-cont-sect-one-items-item-three position-absolute text-white d-flex flex-column gap-5"
+            ref={homeContParagraph}
+          >
+            <h1 className="home-cont-sect-one-items-item-three-h1">
+              It&apos;s that easy to
+              {' '}
+              <br />
+              {' '}
+              bring a Smile on
+              {' '}
+              <br />
+              Their Faces
+            </h1>
+            <p className="home-cont-sect-one-items-item-three-p fs-4">
+              We don&apos;t ask for much, just help us with what
+              <br />
+              you can-Be it Money, skill or Your time
+            </p>
             <button
               type="button"
-              className="btn bg-white donate-btn"
-              alt="learn more btn"
+              className="home-cont-sect-one-items-item-three-btn btn bg-white p-2 w-50 fs-4"
             >
-              LEARN MORE
-            </button>
-            <button
-              type="button"
-              className="btn bg-white donate-btn"
-              alt="learn more btn"
-            >
-              OUR CERTIFICATES
+              Donate Now
             </button>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="home-cont-sect-two container">
+        <div
+          className="container grid home-cont-sect-two-items gap-5 position-relative"
+          ref={homeAboutContainer}
+        >
+          <img
+            src={Aboutusimg}
+            alt="aboutimg"
+            className="w-100 home-cont-sect-two-items-item-one-img"
+            ref={homeAboutContainer}
+          />
+
+          <div className="d-flex flex-column justify-content-center gap-3 home-cont-sect-two-items-item-two">
+            <h5 className="fs-4">ABOUT US</h5>
+            <h1>
+              Think global,
+              {' '}
+              <br />
+              {' '}
+              Act local.
+            </h1>
+            <p>
+              &ldquo;NayePankh Foundation&ldquo; is a non governmental
+              organisation with a strong desire to help the society and make it
+              a better place for all, by doing everything in our power and to
+              make our vision successful we would require your vital support.
+              Service to mankind is the service to god. Let&apos;s revolutionise
+              the society together!.
+            </p>
+            <div
+              className="d-flex justify-content-between"
+            >
+              <button
+                type="button"
+                className="btn bg-white donate-btn"
+                alt="learn more btn"
+              >
+                LEARN MORE
+              </button>
+              <button
+                type="button"
+                className="btn bg-white donate-btn"
+                alt="learn more btn"
+              >
+                OUR CERTIFICATES
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-cont-sect-three">
+        <div className="">
+          <div>
+            <img src={IMG2} alt="photo2" className="w-100" />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
